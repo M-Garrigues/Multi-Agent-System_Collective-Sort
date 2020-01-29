2264499f4b059c182d111e5de98a2b9012981b71
M-Garrigues <mathieu.garrigues@etu.univ-lyon1.fr>
2020-01-29 13:54:36 +0100

    Created Square

---
+++ Square.py
@@ -0,0 +1,31 @@
+class Square:
+
+    def __init__(self):
+        self._has_agent = False
+        self._has_object = False
+        self._occupant = None
+
+    def has_agent(self):
+        return self._has_agent
+
+    def has_object(self):
+        return self._has_object
+
+    def get_occupant(self):
+        return self._occupant
+
+    def set_occupant(self, occupant):
+        assert (not self._has_object and not self._has_agent)
+
+        self._occupant = occupant
+        if type(occupant).__name__ == "Agent":
+            self._has_agent = True
+        else:
+            self._has_object = True
+
+    def take_occupant(self):
+        temp_occupant = self._occupant
+        self._occupant = None
+        self._has_agent = False
+        self._has_object = False
+        return temp_occupant
